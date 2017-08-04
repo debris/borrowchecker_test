6b84b52e7822ff5028d1d76900a6fb2ba1a5f9a1
debris <marek.kotewicz@gmail.com>
2017-08-04 19:15:10 +0200

    added examples

---
+++ foo2/src/lib.rs
@@ -0,0 +1,21 @@
+pub struct Foo {
+	pub data: [u8; 64],
+}
+
+pub struct FooRef<'a> {
+	pub data: &'a [u8; 64],
+}
+
+impl<'a> From<&'a Foo> for FooRef<'a> {
+	fn from(foo: &'a Foo) -> Self {
+		FooRef {
+			data: &foo.data,
+		}
+	}
+}
+
+impl Foo {
+	pub fn do_something<F>(&self, _foo: F) {
+		let _test_foo = FooRef::from(self);
+	}
+}
